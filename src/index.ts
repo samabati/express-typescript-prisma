500b455a2929ceb5ff0fbdfba6fd1ea24344f7d8
samabati <117029968+samabati@users.noreply.github.com>
2024-08-06 15:03:17 -0400

    initializing express app

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(3000, () => console.log("App is running on port 3000"));
