e4b98bd70b598bf13dc6a675e19b1f6708b347ba
samabati <117029968+samabati@users.noreply.github.com>
2024-08-07 08:56:07 -0400

    Route definitions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
+import rootRouter from "./routes";
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working");
-});
+app.use("/api/v1", rootRouter);
 
 app.listen(PORT, () => console.log("App is running on port 3000"));
