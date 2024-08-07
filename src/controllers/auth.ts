9fd82b7223f0aeee764623de642416216149ce48
samabati <117029968+samabati@users.noreply.github.com>
2024-08-07 10:32:19 -0400

    user auth with jwt

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { prismaClient } from "../index";
-import { hashSync } from "bcrypt";
+import { compareSync, hashSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
 
 export const signup = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -19,4 +21,26 @@ export const signup = async (req: Request, res: Response) => {
   });
 
   res.json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    throw Error("user does not exist");
+  }
+
+  if (!compareSync(password, user.password)) {
+    throw Error("password don't match");
+  }
+
+  const token = jwt.sign(
+    {
+      userid: user.id,
+    },
+    JWT_SECRET
+  );
+
+  res.json({ user, token });
 };
