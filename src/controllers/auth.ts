aece3c8a7a39d8533dee5fa890fe62d7dc831539
samabati <117029968+samabati@users.noreply.github.com>
2024-08-07 09:40:49 -0400

    user signup system

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from "express";
+import { prismaClient } from "../index";
+import { hashSync } from "bcrypt";
 
-export const login = (req: Request, res: Response) => {
-  res.send("Login works");
+export const signup = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+
+  if (user) {
+    throw Error("User already exists in database!");
+  }
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+    },
+  });
+
+  res.json(user);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
 
 const app: Express = express();
+
+export const prismaClient = new PrismaClient({
+  log: ["query"],
+});
+
+app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
