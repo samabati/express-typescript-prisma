fd4eaf8a0ff37b42dfb7501397bb7ea5028202c8
samabati <117029968+samabati@users.noreply.github.com>
2024-08-08 15:05:35 -0400

    Added Address model in prisma schema

--- src/errorhandler.ts
+++ src/errorhandler.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { ErrorCode, HttpException } from "./exceptions/root";
 import { InternalException } from "./exceptions/internal-exception";
+import { ZodError } from "zod";
+import { BadException } from "./exceptions/bad-exception";
 
 export const errorHandler = (method: Function) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -10,6 +12,11 @@ export const errorHandler = (method: Function) => {
       let exception: HttpException;
       if (err instanceof HttpException) {
         exception = err;
+      } else if (err instanceof ZodError) {
+        exception = new BadException(
+          "Unprocessable entity",
+          ErrorCode.UNPROCESSABLE_ENTITY
+        );
       } else {
         exception = new InternalException(
           "Something went wrong...",
