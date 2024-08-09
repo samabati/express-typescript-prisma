e50a94d36f5b0bd9e724ff26bd928e1f3188625b
samabati <117029968+samabati@users.noreply.github.com>
2024-08-09 16:39:27 -0400

    Finishing all order routes

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const createOrder = async (req: Request, res: Response) => {
   return await prismaClient.$transaction(async (tx) => {
@@ -62,4 +64,58 @@ export const createOrder = async (req: Request, res: Response) => {
 
     return res.json(order);
   });
+};
+
+export const listOrders = async (req: Request, res: Response) => {
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: req.user.id,
+    },
+  });
+
+  res.json(orders);
+};
+
+export const cancelOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.update({
+      where: {
+        id: +req.params.id,
+      },
+      data: {
+        status: "CANCELED",
+      },
+    });
+
+    const createOrderEvent = await prismaClient.orderEvent.create({
+      data: {
+        orderId: order.id,
+        status: "CANCELED",
+      },
+    });
+
+    res.json(order);
+  } catch (err) {
+    throw new NotFoundException(
+      "Could not find order",
+      ErrorCode.ORDER_NOT_FOUND
+    );
+  }
+};
+
+export const listOrderById = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.findFirstOrThrow({
+      where: {
+        id: +req.params.id,
+      },
+    });
+
+    res.json(order);
+  } catch (err) {
+    throw new NotFoundException(
+      "Could not find order",
+      ErrorCode.ORDER_NOT_FOUND
+    );
+  }
 };
--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { errorHandler } from "../errorhandler";
-import { createOrder } from "../controllers/orders";
+import {
+  cancelOrder,
+  createOrder,
+  listOrderById,
+  listOrders,
+} from "../controllers/orders";
 
 const orderRouter = Router();
 
 orderRouter.post("/", [authMiddleware], errorHandler(createOrder));
+orderRouter.get("/", [authMiddleware], errorHandler(listOrders));
+orderRouter.get("/:id", [authMiddleware], errorHandler(listOrderById));
+orderRouter.delete("/:id", [authMiddleware], errorHandler(cancelOrder));
 
 export default orderRouter;
