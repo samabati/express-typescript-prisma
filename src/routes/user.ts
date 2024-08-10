c9dd39e04ce3552a0b9d9f571c153ec4e65e4e00
samabati <117029968+samabati@users.noreply.github.com>
2024-08-10 08:51:15 -0400

    Admin API endpoints for users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,31 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { errorHandler } from "../errorhandler";
-import { updateUser } from "../controllers/users";
+import {
+  getUserById,
+  listUsers,
+  updateUser,
+  updateUserRole,
+} from "../controllers/users";
+import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const userRouter = Router();
 
 userRouter.put("/", [authMiddleware], errorHandler(updateUser));
+userRouter.put(
+  "/:id/role",
+  [authMiddleware, adminMiddleware],
+  errorHandler(updateUserRole)
+),
+  userRouter.get(
+    "/",
+    [authMiddleware, adminMiddleware],
+    errorHandler(listUsers)
+  ),
+  userRouter.get(
+    "/:id",
+    [authMiddleware, adminMiddleware],
+    errorHandler(getUserById)
+  );
 
 export default userRouter;
